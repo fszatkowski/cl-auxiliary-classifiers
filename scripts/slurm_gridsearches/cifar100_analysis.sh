404755e2ca9a6192618b1d35478517cc16784290
Filip Szatkowski <fmszatkowski@gmail.com>
2024-08-23 10:24:13 +0200

    Add joint to analysis

--- scripts/slurm_gridsearches/cifar100_analysis.sh
+++ scripts/slurm_gridsearches/cifar100_analysis.sh
@@ -21,6 +21,9 @@ for seed in 0 1 2; do
         num_exemplars=2000
         sbatch -A plgdynamic2-gpu-a100 -p plgrid-gpu-a100 scripts/templates/cifar100_analysis/ft_ee.sh ${num_tasks} ${seed} ${num_exemplars} ${ic_config}
 
+        # Joint
+        sbatch -A plgdynamic2-gpu-a100 -p plgrid-gpu-a100 scripts/templates/cifar100_analysis/joint_ee.sh ${num_tasks} ${seed} ${ic_config}
+
         ic_config=cifar100_resnet32_sdn_detach
         # FT
         num_exemplars=0
@@ -29,5 +32,8 @@ for seed in 0 1 2; do
         # FT+Ex
         num_exemplars=2000
         sbatch -A plgdynamic2-gpu-a100 -p plgrid-gpu-a100 scripts/templates/cifar100_analysis/ft_ee.sh ${num_tasks} ${seed} ${num_exemplars} ${ic_config}
+
+        # Joint
+        sbatch -A plgdynamic2-gpu-a100 -p plgrid-gpu-a100 scripts/templates/cifar100_analysis/joint_ee.sh ${num_tasks} ${seed} ${ic_config}
     done
 done
